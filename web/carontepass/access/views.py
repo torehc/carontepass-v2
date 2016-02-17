745d391973bf43d0b84cb96951ea4a080a545596
torehc <torehc@gmail.com>
2016-02-17 13:14:54 +0000

    Login required in pages

--- web/carontepass/access/views.py
+++ web/carontepass/access/views.py
@@ -5,6 +5,8 @@ from .serializers import DeviceResultSerializer
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
@@ -20,24 +22,26 @@ class DeviceIDList(generics.ListAPIView):
         
         return Device.objects.filter(code=code_id)
         
-
+        
+@login_required(login_url='/')
 def homepage(request):
     users_count = User.objects.count()
     users_in_count = Log.listUsersCount()
     return render(request, 'access/index.html', {'users_count': users_count, 'users_in_count': users_in_count})
     
 
+@login_required(login_url='/')    
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/')
-    
+
+
+@login_required(login_url='/')
 def personal_info(request):
     return render(request, 'access/info.html')
 
-"""
-def device_info(request):
-    return render(request, 'access/devicelist.html')    
-"""
+
+@login_required(login_url='/')
 def device_info(request):
     device_list_user = Device.objects.filter(user=User).all()
     return render(request, 'access/devicelist.html', {'device_list_user': device_list_user})
