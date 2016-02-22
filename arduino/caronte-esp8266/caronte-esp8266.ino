20737b65a44ccb799a665211526afeba7af48dc5
Torehc <torehc@gmail.com>
2016-02-22 19:55:48 +0000

    Code for the client, ESP8266 with Arduino Core

---
+++ arduino/caronte-esp8266/caronte-esp8266.ino
@@ -0,0 +1,35 @@
+#include "MFRC522.h"
+#define RST_PIN 16 // RST-PIN for RC522 - RFID - SPI - Modul GPIO16 
+#define SS_PIN  15  // SDA-PIN for RC522 - RFID - SPI - Modul GPI15 
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
+
+void setup() {
+  Serial.begin(9600);    // Initialize serial communications
+  SPI.begin();           // Init SPI bus
+  mfrc522.PCD_Init();    // Init MFRC522
+}
+
+void loop() { 
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    delay(50);
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    delay(50);
+    return;
+  }
+  // Show some details of the PICC (that is: the tag/card)
+  Serial.print(F("Card UID:"));
+  dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
+  Serial.println();
+}
+
+// Helper routine to dump a byte array as hex values to Serial
+void dump_byte_array(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? "" : ".");
+    Serial.print(buffer[i], DEC);
+  }
+}
