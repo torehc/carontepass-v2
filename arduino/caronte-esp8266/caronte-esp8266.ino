51c101de9af7249ffb172d701d7011ed14e8b4b4
Torehc <torehc@gmail.com>
2016-02-23 20:46:44 +0000

    Added support wifi on esp8266, with authentication for django api rest

--- arduino/caronte-esp8266/caronte-esp8266.ino
+++ arduino/caronte-esp8266/caronte-esp8266.ino
@@ -1,12 +1,59 @@
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h> //TODO
 #include "MFRC522.h"
 #define RST_PIN 16 // RST-PIN for RC522 - RFID - SPI - Modul GPIO16 
 #define SS_PIN  15  // SDA-PIN for RC522 - RFID - SPI - Modul GPI15 
+
+const char* ssid     = "your-ssid";
+const char* password = "your-password";
+
+const char* host = "server-ip";
+
+/*
+ * For User Auhentication:
+ * https://en.wikipedia.org/wiki/Basic_access_authentication
+ * Use user:password in base64
+ * Ej: https://webnet77.net/cgi-bin/helpers/base-64.pl
+*/
+
+const char* userpass = "xxxx";
+
+int tag[4];
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 
+void dump_byte_array(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? "" : ".");
+    Serial.print(buffer[i], DEC);
+    tag[i] = buffer[i];
+  }
+}
+
 void setup() {
-  Serial.begin(9600);    // Initialize serial communications
+  
+  // Initialize serial communications
+  Serial.begin(115200);
+  delay(10);
   SPI.begin();           // Init SPI bus
   mfrc522.PCD_Init();    // Init MFRC522
+  
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
 }
 
 void loop() { 
@@ -24,12 +71,61 @@ void loop() {
   Serial.print(F("Card UID:"));
   dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
   Serial.println();
-}
 
-// Helper routine to dump a byte array as hex values to Serial
-void dump_byte_array(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? "" : ".");
-    Serial.print(buffer[i], DEC);
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 8000;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
   }
+  
+  // We now create a URI for the request
+  String url = "/api/1/device/";
+  url += tag[0];
+  url += ".";
+  url += tag[1];
+  url += ".";
+  url += tag[2];
+  url += ".";
+  url += tag[3];
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  client.println(
+    String("GET ") + url + " HTTP/1.1\r\n" + 
+    "Host: " + host + "\r\n" + "Authorization: Basic " + userpass + "\r\n" + 
+    "Connection: close\r\n\r\n");
+               
+  delay(10);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+   
+  /*
+      StaticJsonBuffer<200> jsonBuffer;
+      
+      JsonObject& root = jsonBuffer.parseObject(c);
+      
+      int id_device = root["id"];
+      int id_user = root["user"];
+      const char* tag_type = root["kind"];
+      const char* tag_code = root["code"];
+      const char* result = root["result"];
+*/
+  
+  Serial.println();
+  Serial.println("closing connection");
+
+  delay(5000);
+  
 }
