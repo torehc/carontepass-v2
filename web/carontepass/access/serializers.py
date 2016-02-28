5d3f24cb05139850d851828fc9b4749ca9d3ab6f
torehc <torehc@gmail.com>
2016-02-28 00:19:01 +0000

    Added grace period. It sets margin a few days between the beginning of the month and the date on which the monthly payments are charged.

--- web/carontepass/access/serializers.py
+++ web/carontepass/access/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from carontepass.settings import VALUE_PAYMENT_TRUE
+from carontepass.settings import VALUE_PAYMENT_TRUE, MAX_GRANTED_DAYS
 from .models import Device, Payment, Log
 import datetime
 from django.contrib.auth.models import User
@@ -21,13 +21,23 @@ class DeviceResultSerializer(serializers.ModelSerializer):
     
     def is_auth_user(self, Device):
         
+        #Check if the user has monthly payments
         month_actual = datetime.datetime.now().month
         
         if Payment.objects.filter(user=Device.user, month=month_actual): 
             if  Payment.objects.filter(user=Device.user, month=month_actual)[0].amount >= VALUE_PAYMENT_TRUE:
                 
                 Log.checkentryLog(Device)
+                return True;
                 
+        #Grace period for the first day of the month
+        if not Device.kind == "tag":
+            #The tags that have no assigned user are exempt from the days of courtesy.
+            day_actual = datetime.datetime.now().day
+            
+            if datetime.datetime.now().day <= MAX_GRANTED_DAYS:
+                
+                Log.checkentryLog(Device)
                 return True;
                 
     class Meta:
