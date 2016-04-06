f4de07d31ad5ffd3ec07ca9bcb138fc33ce67eb3
torehc <torehc@gmail.com>
2016-04-06 12:20:25 +0000

    Visual improvement of the groups and users in the administration part

--- web/carontepass/access/admin.py
+++ web/carontepass/access/admin.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
 from access.models import Message, Payment, Device, Log, Telegram
+
+from django.utils.safestring import mark_safe
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
 
 
 class DeviceAdmin(admin.ModelAdmin): 
@@ -28,9 +33,46 @@ class MessageAdmin(admin.ModelAdmin):
 class TelegramAdmin(admin.ModelAdmin): 
   list_display = ('user', 'chatid') 
   search_fields = ('user', 'chatid') 
+  
+def roles(self):
+    #short_name = unicode # function to get group name
+    short_name = lambda x:unicode(x)[:1].upper() # first letter of a group
+    p = sorted([u"<a title='%s'>%s</a>" % (x, short_name(x)) for x in self.groups.all()])
+    if self.user_permissions.count(): p += ['+']
+    value = ', '.join(p)
+    return mark_safe("<nobr>%s</nobr>" % value)
+roles.allow_tags = True
+roles.short_description = u'Groups'
+
+def adm(self):
+    return self.is_superuser
+adm.boolean = True
+adm.admin_order_field = 'is_superuser'
+
+def staff(self):
+    return self.is_staff
+staff.boolean = True
+staff.admin_order_field = 'is_staff'
+
+from django.core.urlresolvers import reverse
+def persons(self):
+    return ', '.join(['<a href="%s">%s</a>' % (reverse('admin:auth_user_change', args=(x.id,)), x.username) for x in self.user_set.all().order_by('username')])
+persons.allow_tags = True
+
+class UserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_active', staff, adm, roles]
+    list_filter = ['groups', 'is_staff', 'is_superuser', 'is_active']
+
+class GroupAdmin(GroupAdmin):
+    list_display = ['name', persons]
+    list_display_links = ['name']
 
 
 # Register your models here.
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
+admin.site.register(Group, GroupAdmin)
 admin.site.register(Message, MessageAdmin)
 admin.site.register(Payment, PaymentAdmin)
 admin.site.register(Device, DeviceAdmin)
