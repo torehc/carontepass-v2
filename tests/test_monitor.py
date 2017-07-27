7bd3febfe553e1f2d512b6f7dea0b88708e542ab
Victor Suarez <suarez.garcia.victor@gmail.com>
2017-07-27 18:30:56 +0100

    Se ha modificado el test_monitor para que obtenga los datos desde la configuracion

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -1,17 +1,22 @@
 
 from http import client
-import os
 
-RPI_IP='192.168.1.94'
-CARONTE_IP='192.168.1.21'
+import sys
+sys.path.append('../web/carontepass/')
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carontepass.settings")
+import django
+django.setup()
+from carontepass.settings_local import ESP_IP, RASPBERY_IP
+
 
 def test_raspberrypi():
-    h1=client.HTTPConnection(RPI_IP,80)
-    h1.request('GET','/')
+    h1=client.HTTPConnection(RASPBERY_IP, 80)
+    h1.request('GET', '/')
     conn = h1.getresponse()
     assert conn.status==200
 
 def test_caronteclient():
-    hostname = CARONTE_IP
+    hostname = ESP_IP
     response = os.system("ping -c 1 " + hostname + " > /dev/null")
     assert response == 0
