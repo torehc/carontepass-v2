7ba5af527ec923b6e17c47243eb80128b87be702
Victor Suarez <suarez.garcia.victor@gmail.com>
2017-07-31 17:56:26 +0100

    Añadiendo test de monitorizacion de la API Rest.

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -7,7 +7,7 @@ print(os.getcwd())
 
 import sys
 sys.path.append('../')
-from web.carontepass.carontepass.settings_local import ESP_IP, RASPBERY_IP
+from web.carontepass.carontepass.settings_local import ESP_IP, RASPBERY_IP,USER_PASS_AUTH
 
 
 def test_raspberrypi():
@@ -20,3 +20,13 @@ def test_caronteclient():
     hostname = ESP_IP
     response = os.system("ping -c 1 {} > /dev/null".format(hostname))
     assert response == 0
+
+def test_caronterest():
+    host=RASPBERY_IP
+    url='http://{}/api/1/device/'.format(host)
+    userpass=USER_PASS_AUTH
+    h1=client.HTTPConnection(url,80)
+    headers = { 'Authorization' : 'Basic %s' %  userpass }
+    c.request('GET', '/', headers=headers)
+    res=c.getresponse()
+    assert c.status=200
